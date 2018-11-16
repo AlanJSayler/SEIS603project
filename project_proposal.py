f483c9a6c5eb84d8c725137116cbbc33eff6c24e
AlanJSayler <alanjsayler@gmail.com>
2018-11-16 15:43:26 -0600

    add project proposal file

---
+++ project_proposal.py
@@ -0,0 +1,30 @@
+# Project Proposal:  Get abstract of research publications from MIT Computer Science faculty.
+
+# Project Team7: Alan Sayler, Saritha Gajella, Adebola Onikoyi
+
+
+
+
+"""
+Design and implement a tool in python for - getting abstract of all
+the research publication of faculty at MIT computer science.
+Faculty website https://www.eecs.mit.edu/people/faculty-advisors.
+Research publication at https://arxiv.org.
+
+Learning objectives:
+0) Preparing the data set for feeding to machine learning algorithms
+   for data analysis.
+1) Learn python pkgs such as bs4 - web scrapping tool.
+2) Learn basics of HTML pages.
+3) Learn python pkg such as urllib - open a website, read the contents etc.
+4) Learn code collaboration tools such as github (and git).
+
+
+Tasks:
+1) Learn bs4 pkg
+2) Learn HTML pages, tags etc.
+3) Learn urllib pkg.
+4) Write a function to get the list of professor names from the website.
+5) Given a professor name, write a function to get the abstracts from https://arxiv.org
+
+"""
