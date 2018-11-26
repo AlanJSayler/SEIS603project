ca0c5bd34f969b55606efe2ffb8ee9c843cf7147
AlanJSayler <alanjsayler@gmail.com>
2018-11-25 20:39:48 -0600

    got list of authors

--- scraper.py
+++ scraper.py
@@ -1,2 +1,13 @@
-print("Hi Ade")
-print("Hi Alan")
+import urllib.request
+from bs4 import BeautifulSoup
+
+with urllib.request.urlopen("http://www.eecs.mit.edu/people/faculty-advisors") as response:
+    html = response.read()
+
+authors = []
+soup = BeautifulSoup(html, "html.parser")
+for field_content in soup.find_all('span', {'class':'field-content card-title'}):
+    authors.append(field_content.text.strip())
+
+print(authors)
+
