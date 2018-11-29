853181884f979f76fafec6a3152d6e3f1011784d
AlanJSayler <alanjsayler@gmail.com>
2018-11-28 18:51:09 -0600

    modularized scrape functions

--- scraper.py
+++ scraper.py
@@ -1,13 +1,24 @@
-import urllib.request
+from urllib import request
 from bs4 import BeautifulSoup
 
-with urllib.request.urlopen("http://www.eecs.mit.edu/people/faculty-advisors") as response:
-    html = response.read()
 
-authors = []
-soup = BeautifulSoup(html, "html.parser")
-for field_content in soup.find_all('span', {'class':'field-content card-title'}):
-    authors.append(field_content.text.strip())
+#Scrapes the requested url and returns a list of all text found within the specified
+#span or div class
+def scrape(url,getType,getClass):
+    with request.urlopen(url) as response:
+        html = response.read()
+    soup = BeautifulSoup(html, "html.parser")
+    results = []
+    for field_content in soup.findAll(getType, {'class' : getClass}):
+        results.append(field_content.text.strip())
+    return results
 
-print(authors)
+
+
+
+
+
+
+
+
 
