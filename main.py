68b3b06611b1d4ed2338749cfb7621a022570f98
AlanJSayler <alanjsayler@gmail.com>
2018-11-30 14:31:55 -0600

    refactored for conciseness

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 #Alan
 #Ade
 #Saritha
-from arxiv import getAbstracts
-from mit import getFacultyAdvisors
+from scraper import scrape
 from json import dumps
+from urllib import parse
 
-faculty = getFacultyAdvisors()
+faculty = scrape("http://www.eecs.mit.edu/people/faculty-advisors", "span", "field-content card-title")
 articles = []
 for advisor in faculty:
-    for abstract in getAbstracts(advisor,"author"):
+    args = {
+        "query" : advisor,
+        "searchtype" : "author",
+        "abstracts" : "show"
+    }
+    for abstract in scrape("https://arxiv.org/search/?{}".format(parse.urlencode(args)),
+                           "span",
+                           "abstract-full has-text-grey-dark mathjax"):
         entry = {"author" : advisor,
                  "abstract" : abstract}
         articles.append(entry)
 
 print(articles)
-with open ('articles.json', 'w') as file:
+with open('articles.json', 'w') as file:
     file.write(dumps(articles))
--- mit.py
+++
@@ -1,9 +0,0 @@
-#Alan
-#Ade
-#Saritha
-from scraper import scrape
-
-#return faculty advisors from MIT eecs.
-def getFacultyAdvisors():
-    return scrape("http://www.eecs.mit.edu/people/faculty-advisors", "span", "field-content card-title")
-
