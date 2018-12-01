c8e769fd6dcc05aae0537c762beec45bab2be92a
AlanJSayler <alanjsayler@gmail.com>
2018-11-30 18:02:29 -0600

    removed holding abstracts in memory

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ from json import dumps
 from urllib import parse
 
 faculty = scrape("http://www.eecs.mit.edu/people/faculty-advisors", "span", "field-content card-title")
-articles = []
-for advisor in faculty:
-    args = {
-        "query" : advisor,
-        "searchtype" : "author",
-        "abstracts" : "show"
-    }
-    for abstract in scrape("https://arxiv.org/search/?{}".format(parse.urlencode(args)),
-                           "span",
-                           "abstract-full has-text-grey-dark mathjax"):
-        entry = {"author" : advisor,
-                 "abstract" : abstract}
-        articles.append(entry)
-
-print(articles)
 with open('articles.json', 'w') as file:
-    file.write(dumps(articles))
+    for advisor in faculty:
+        args = {
+            "query" : advisor,
+            "searchtype" : "author",
+            "abstracts" : "show"
+        }
+        for abstract in scrape("https://arxiv.org/search/?{}".format(parse.urlencode(args)),
+                               "span",
+                               "abstract-full has-text-grey-dark mathjax"):
+            entry = {"author" : advisor,
+                     "abstract" : abstract}
+            print(entry)
+            file.write(dumps(entry) + "\n")
+
+
+
