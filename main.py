c80120c698e9057b627a5446a53c2882bfae805d
AlanJSayler <alanjsayler@gmail.com>
2018-12-03 17:42:22 -0600

    made optional abstract vs title

--- main.py
+++ main.py
@@ -6,20 +6,41 @@ from json import dumps
 from urllib import parse
 
 faculty = scrape("http://www.eecs.mit.edu/people/faculty-advisors", "span", "field-content card-title")
+goodInput = False
+while not goodInput:
+    goodInput = True
+    try:
+        mode = int(input("Welcome to the mit scraper app, press 1 to scrape article titles, or 2 to scrape abstracts: "))
+    except:
+        print("Cannot cast input to int, try again")
+        goodInput = False
+        continue
+    if(mode == 1):
+        arxivArgs = {"element":"p",
+                "class":"title is-5 mathjax",
+                "mode":"title"}
+    elif(mode == 2):
+        arxivArgs = {"element":"span",
+                "class":"abstract-full has-text-grey-dark mathjax",
+                "mode":"abstract"}
+    else:
+        print("sorry, that wasn't a 1 or 2")
+        goodInput = False
+
 with open('articles.json', 'w') as file:
     for advisor in faculty:
-        args = {
+        mitArgs = {
             "query" : advisor,
             "searchtype" : "author",
             "abstracts" : "show"
         }
-        for abstract in scrape("https://arxiv.org/search/?{}".format(parse.urlencode(args)),
-                               "span",
-                               "abstract-full has-text-grey-dark mathjax"):
+        for abstract in scrape("https://arxiv.org/search/?{}".format(parse.urlencode(mitArgs)),
+                               arxivArgs.get("element",""),
+                               arxivArgs.get("class","")):
             entry = {"author" : advisor,
-                     "abstract" : abstract}
+                     arxivArgs.get("mode","") : abstract}
             print(entry)
-            file.write(dumps(entry) + "\n")
+            file.write(dumps(entry, indent = 4) + "\n",)
 
 
 
